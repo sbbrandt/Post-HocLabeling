f80594cddcdcfe94eac3a9f4d6f1bf7ba3a16085
Simon Brandt <simonbrandt@protonmail.com>
2019-08-08 14:02:30 +0200

    updated auxillary to read .ini files containing [config] header. added propertylist2struct.m to src

--- src/auxiliary_generate4Python.m
+++ src/auxiliary_generate4Python.m
@@ -1,8 +1,17 @@
 function auxiliary_generate4Python(cfg_file)
 addpath('~/libraries')
 set_localpaths();
-
-cfg = ini2struct(cfg_file)
+cfg = ini2struct(cfg_file);
+if strcmp(fieldnames(cfg),'config')
+    cfg = cfg.config;
+end
+%make str entries num
+fn = fieldnames(cfg);
+for ii=1:numel(fn)
+    if ~isempty(str2num(cfg.(fn{ii})))
+        cfg.(fn{ii}) = str2num(cfg.(fn{ii}));
+    end
+end
 
 assert(diff(cfg.fband) > 0)
 fc = mean(cfg.fband);
@@ -13,7 +22,15 @@ power_quantile = cfg.power_quantile;
 discrete_quantile = cfg.discrete_quantiles;
 eeg_data_file=cfg.eeg_data_file;
 file_name_save=cfg.file_name_save;
+%Ne = cfg.ne;
+%eta = cfg.eta;
 
+%% get random from .../S1-S7, if path is .../random
+%if ismember('random',split(eeg_data_file,'/'))
+%    eeg_data_file = join([eeg_data_file(1:end-6),'S',string(randi(7))],'');
+%end
+
+%%
 options = struct(...
     'file_headModel', fullfile('data/sa_nyhead_simplified.mat'), ...
     'ford', 5, ...
@@ -22,11 +39,13 @@ options = struct(...
     'windowLength', winlength, ...
     'N_compICA', 20,...
     'type', type, ...
+    'select_sources', 'all', ...
     'power_quartile',power_quantile,...
     'discrete_quantiles', discrete_quantile);
     %'select_sources', 'all');
 
 window_offset = [0,0];
+
 eeg_data = load(eeg_data_file);
 
 % skip the first and last n events
@@ -37,18 +56,33 @@ ntotal = length(eeg_data.vmrk.y);
 
 [epo_sources, Ax] = load_simulation(eeg_data, options);
 
-
+%%
 vmrk = mrk_selectEvents(eeg_data.vmrk, 'not', eeg_data.iart);
 [epo, complete]= proc_segmentation(eeg_data.cnt, vmrk, [window_offset(1),winlength + window_offset(2)]);
+
 epo_sources = proc_selectEpochs(epo_sources,complete);
 
 cnt=eeg_data.cnt;
 vmrk = mrk_selectEvents(vmrk,complete);
-
+% select data length
+% if Ne~=0
+%     epo_sources.x = epo_sources.x(:,:,1:Ne);
+%     epo_sources.y = epo_sources.y(1:Ne);
+%     epo_sources.event.desc(end+1-Ne:end);
+%     epo_sources.mrk_info.event.desc(end+1-Ne:end);
+%     vmrk.y = vmrk.y(1:Ne);
+%     vmrk.time = vmrk.time(1:Ne);
+%     vmrk.event.desc(end+1-Ne:end);
+% end
+% add noise
+% if eta~=0
+%     epo_sources.x = epo_sources.x + (1-(1-eta)^2)/(1-eta)^2 ...
+%         *var(mean(epo_sources.x))*randn(1,1,size(epo_sources.x,3));
+% end
+%%
 save(file_name_save,'cnt','vmrk','epo_sources','window_offset', 'winlength');
 %ntrials = length(eeg_data.vmrk.time);
 %eeg_data.iart = unique([1,2,n_trials-1,n_trials-2,eeg_data.iart])
 
 %[epo, complete]= proc_segmentation(eeg_data.cnt, vmrk, [-500,winlength+500]);
 return
-
